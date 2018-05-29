126311a67272eab95438e74140091c28605d86ba
no <no>
2018-05-29 09:35:25 +0300

    fix

--- index.js
+++ index.js
@@ -7,24 +7,7 @@ const parseConfig = require('./parseConfig');
 const getXdgUserDirs = () => {
     const configHome =
         process.env.XDG_CONFIG_HOME || join(process.env.HOME, '.config');
-    return readFile(join(configHome, 'user-dirs.dirs')).then(text => {
-        const config = parseConfig(text);
-        console.log(config);
-
-        assert(config.XDG_DESKTOP_DIR, 'XDG_DESKTOP_DIR missing');
-        assert(config.XDG_DOCUMENTS_DIR, 'XDG_DOCUMENTS_DIR missing');
-        assert(config.XDG_DOWNLOAD_DIR, 'XDG_DOWNLOAD_DIR missing');
-        assert(config.XDG_MUSIC_DIR, 'XDG_MUSIC_DIR missing');
-        assert(config.XDG_PICTURES_DIR, 'XDG_PICTURES_DIR missing');
-        assert(config.XDG_PUBLICSHARE_DIR, 'XDG_PUBLICSHARE_DIR missing');
-        assert(config.XDG_TEMPLATES_DIR, 'XDG_TEMPLATES_DIR missing');
-        assert(config.XDG_VIDEOS_DIR, 'XDG_VIDEOS_DIR missing');
-
-        assert(
-            config.XDG_DESKTOP_DIR === process.env.HOME + '/Desktop' ||
-                config.XDG_DESKTOP_DIR === process.env.HOME + '/Рабочий стол'
-        );
-    });
+    return readFile(join(configHome, 'user-dirs.dirs')).then(parseConfig);
 };
 
 module.exports = getXdgUserDirs;
--- test.js
+++ test.js
@@ -6,4 +6,25 @@ process.env.FOO = 'bar';
 assert.equal(processEnvVars('$FOO-becue $FOO'), 'bar-becue bar');
 assert.equal(processEnvVars('${FOO}becue ${FOO}'), 'barbecue bar');
 
-getXdgUserDirs();
+getXdgUserDirs()
+    .then(config => {
+        console.log(config);
+
+        assert(config.XDG_DESKTOP_DIR, 'XDG_DESKTOP_DIR missing');
+        assert(config.XDG_DOCUMENTS_DIR, 'XDG_DOCUMENTS_DIR missing');
+        assert(config.XDG_DOWNLOAD_DIR, 'XDG_DOWNLOAD_DIR missing');
+        assert(config.XDG_MUSIC_DIR, 'XDG_MUSIC_DIR missing');
+        assert(config.XDG_PICTURES_DIR, 'XDG_PICTURES_DIR missing');
+        assert(config.XDG_PUBLICSHARE_DIR, 'XDG_PUBLICSHARE_DIR missing');
+        assert(config.XDG_TEMPLATES_DIR, 'XDG_TEMPLATES_DIR missing');
+        assert(config.XDG_VIDEOS_DIR, 'XDG_VIDEOS_DIR missing');
+
+        assert(
+            config.XDG_DESKTOP_DIR === process.env.HOME + '/Desktop' ||
+                config.XDG_DESKTOP_DIR === process.env.HOME + '/Рабочий стол'
+        );
+    })
+    .catch(e => {
+        console.error(e.stack);
+        process.exit(1);
+    });
